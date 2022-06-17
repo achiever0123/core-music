08727fccdbae2796909e055a57396219838f0667
Jackson Adams <williamjackson0123@outlook.com>
2022-06-17 04:39:32 -0700

    commit enter

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -10,7 +10,7 @@ const AppHeader = () => {
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
-    <CHeader position="sticky" className="mb-4">
+    <CHeader position="sticky" style={{ border: 'none' }} className="mb-4">
       <CContainer fluid>
         <CHeaderToggler
           className="ps-1"
@@ -18,35 +18,7 @@ const AppHeader = () => {
         >
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
-        {/* <CHeaderBrand className="mx-auto d-md-none" to="/">
-          <CIcon icon={logo} height={48} alt="Logo" />
-        </CHeaderBrand> */}
-        <CHeaderNav className="d-none d-md-flex me-auto">
-          {/* <CNavItem>
-            <CNavLink to="/dashboard" component={NavLink}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Users</CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Settings</CNavLink>
-          </CNavItem> */}
-        </CHeaderNav>
-        {/* <CHeaderNav className="d-md-flex">
-          <CInputGroup size="lg">
-            <CFormInput
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="inputGroup-sizing-lg"
-            />
-            <CInputGroupText id="inputGroup-sizing-lg">@</CInputGroupText>
-          </CInputGroup>
-          <CButton color="primary" size="lg">
-            Large button
-          </CButton>
-        </CHeaderNav> */}
+        <CHeaderNav className="d-none d-md-flex me-auto"></CHeaderNav>
         <CHeaderNav>
           <CNavItem>
             <CNavLink href="#">
